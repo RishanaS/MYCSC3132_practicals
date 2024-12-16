e278d964fd2d9e800f8d4354ae55e15387a1eadf
RishanaS <139629595+RishanaS@users.noreply.github.com>
2024-12-16 10:38:43 +0000

    conf.php file created

---
+++ 13.12.2024/conf.php
@@ -0,0 +1,17 @@
+<?php
+	define('HOST','localhost');
+	define('USERNAME','root');
+	define('PWD','');
+	define('DB','icae02');
+
+	/*
+	define('SERVERNAME', '127.0.0.1:3306');
+	define('USERNAME', 'root');
+	define('PASSWORD', 'mariadb');
+	define('DBNAME', 'icae02');
+	
+	
+	*/
+
+	$connection = mysqli_connect(HOST,USERNAME,PWD,DB);
+?>
