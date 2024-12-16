cee9e4000bd9afed4b8505ec9381c17e32988b64
RishanaS <139629595+RishanaS@users.noreply.github.com>
2024-12-16 11:03:34 +0000

    CODE CHANGED

--- 13.12.2024/conf.php
+++ 13.12.2024/conf.php
@@ -1,17 +1,14 @@
 <?php
-	define('HOST','localhost');
-	define('USERNAME','root');
-	define('PWD','');
-	define('DB','icae02');
 
-	/*
-	define('SERVERNAME', '127.0.0.1:3306');
+
+
+	define('HOST', '127.0.0.1:3306');
 	define('USERNAME', 'root');
-	define('PASSWORD', 'mariadb');
-	define('DBNAME', 'icae02');
+	define('PWD', 'mariadb');
+	define('DB', 'icae02');
 	
 	
-	*/
+
 
 	$connection = mysqli_connect(HOST,USERNAME,PWD,DB);
 ?>
